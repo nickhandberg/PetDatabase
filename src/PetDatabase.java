9b3e0b44913ff4334c9048ec32b779bc25a11d76
noko12312 <42224565+noko12312@users.noreply.github.com>
2021-11-02 15:07:57 -0500

    Test commit

---
+++ src/PetDatabase.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PetDatabase {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int choice = 0;
+
+        System.out.println("Test");
+    }
+}
